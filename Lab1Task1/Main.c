ee2ede296f32de8285da7a6168a811ac7e0dab83
willmayes <willmayes@WILLMAYES4E62>
2023-08-31 08:50:22 -0700

    Add project files.

---
+++ Lab1Task1/Main.c
@@ -0,0 +1,12 @@
+/*******************************************************************************************
+* Programmer: Will Mayes
+* Class: CptS 122, Fall 2023; Lab Section 6
+* Programming Assignment: Lab1Task1
+* Date: August 31, 2023
+* Description: This program prints out a simple "hello world" message.
+*******************************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	printf("Hello World, I am in CptS 122!");
+}
